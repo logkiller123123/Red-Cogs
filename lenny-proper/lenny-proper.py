890bd7c56bda868fcdad7ef03810a27f9379729b
logkiller123123 <nathan_clark28@mymail.eku.edu>
2017-09-18 22:53:18 -0500

    Update

--- lenny-proper/lenny-proper.py
+++ lenny-proper/lenny-proper.py
@@ -1,16 +1,28 @@
-import discord
+from random import choice
+
+import aiohttp
 from discord.ext import commands
 
-class Lenny:
-    """My custom cog that does stuff!"""
 
-  def __init__(self, bot: commands.bot):
+class Lenny:
+
+    """( ͡° ͜ʖ ͡°)"""
+
+    def __init__(self, bot: commands.bot):
         self.bot = bot
 
     @commands.command(pass_context=True, name="lenny")
     async def _lenny(self, ctx: commands.Context):
+        """Displays a random ASCII face."""
 
-        await self.bot.say("( ͡° ͜ʖ ͡°)")
+        url = "http://lenny.today/api/v1/random?limit=1"
+        async with aiohttp.get(url) as response:
+            lenny = (await response.json())[0]["face"]
+            lenny = lenny.replace("*", "\*").replace(
+                "`", "\`").replace("_", "\_").replace("~", "\~")
+
+            await self.bot.say(lenny)
+
 
 def setup(bot: commands.bot):
     bot.add_cog(Lenny(bot))
